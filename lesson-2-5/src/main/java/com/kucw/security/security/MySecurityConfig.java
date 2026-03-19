3f40b3416cfc814ae25bf32e645663eadbc952a0
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-19 16:08:08 +0800

    feat: 單元 6 - 使用資料庫儲存帳號密碼

---
+++ lesson-2-5/src/main/java/com/kucw/security/security/MySecurityConfig.java
@@ -0,0 +1,20 @@
+package com.kucw.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
+}
