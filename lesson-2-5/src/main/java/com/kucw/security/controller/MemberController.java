7c06ddec673dba6196bd462db03e1e9509c12444
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-19 16:53:47 +0800

    feat: 單元 8 - 自定義 Http request 設定 - HttpSecurity

---
+++ lesson-2-5/src/main/java/com/kucw/security/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.kucw.security.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.kucw.security.dao.MemberDao;
+import com.kucw.security.model.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class MemberController {
+
+  @Autowired
+  private MemberDao memberDao;
+
+  @PostMapping("/register")
+  public Member register(@RequestBody Member member) {
+
+    // 先 pass 參數檢查，因為跟 Spring Security
+    Integer memberId = memberDao.createMember(member);
+
+    return memberDao.getMemberById(memberId);
+  }
+
+
+}
--- lesson-2-5/src/main/java/com/kucw/security/security/MySecurityConfig.java
+++ lesson-2-5/src/main/java/com/kucw/security/security/MySecurityConfig.java
@@ -2,12 +2,15 @@ package com.kucw.security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -17,4 +20,13 @@ public class MySecurityConfig {
   public PasswordEncoder passwordEncoder() {
     return NoOpPasswordEncoder.getInstance();
   }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.csrf(csrf -> csrf.disable()).httpBasic(Customizer.withDefaults())
+        .formLogin(Customizer.withDefaults()).authorizeHttpRequests(request -> request
+            .requestMatchers("/welcome", "/register").permitAll().anyRequest().authenticated())
+        .build();
+  }
+
 }
