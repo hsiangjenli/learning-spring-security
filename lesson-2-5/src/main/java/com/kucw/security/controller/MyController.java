1a6d8680b84bdcb332db8ca9cd2d052271469c78
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-20 09:48:50 +0800

    feat: 單元 12 - 在 api 中取得身份認證的資訊 - Authentication

--- lesson-2-5/src/main/java/com/kucw/security/controller/MyController.java
+++ lesson-2-5/src/main/java/com/kucw/security/controller/MyController.java
@@ -1,5 +1,8 @@
 package com.kucw.security.controller;
 
+import java.util.Collection;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,8 +12,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MyController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    public String hello(Authentication authentication) {
+
+        String username = authentication.getName();
+
+        // 取得使用者的權限
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        return "Hello " + username + "!";
     }
 
     @GetMapping("/welcome")
