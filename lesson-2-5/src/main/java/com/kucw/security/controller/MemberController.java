b95821ce39650f9e9e58079cd8d893fb06862681
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-20 09:39:12 +0800

    feat: 單元 11 - 實作密碼加密

--- lesson-2-5/src/main/java/com/kucw/security/controller/MemberController.java
+++ lesson-2-5/src/main/java/com/kucw/security/controller/MemberController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kucw.security.dao.MemberDao;
 import com.kucw.security.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,8 +15,14 @@ public class MemberController {
   @Autowired
   private MemberDao memberDao;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   @PostMapping("/register")
   public Member register(@RequestBody Member member) {
+
+    String hashedPassword = passwordEncoder.encode(member.getPassword());
+    member.setPassword(hashedPassword);
 
     // 先 pass 參數檢查，因為跟 Spring Security
     Integer memberId = memberDao.createMember(member);
--- lesson-2-5/src/main/java/com/kucw/security/security/MySecurityConfig.java
+++ lesson-2-5/src/main/java/com/kucw/security/security/MySecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -19,7 +20,8 @@ public class MySecurityConfig {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
+    // return NoOpPasswordEncoder.getInstance();
+    return new BCryptPasswordEncoder();
   }
 
   @Bean
