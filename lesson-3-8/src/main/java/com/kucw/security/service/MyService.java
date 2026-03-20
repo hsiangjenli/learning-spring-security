2ff5fb724d067fe2a0989a1752bca4db801198d8
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-20 11:54:39 +0800

    chore: 單元 8 - 方法層級的授權 - @PreAuthorize、@PostAuthorize

--- lesson-3-8/src/main/java/com/kucw/security/service/MyService.java
+++ lesson-3-8/src/main/java/com/kucw/security/service/MyService.java
@@ -1,8 +1,13 @@
 package com.kucw.security.service;
 
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface MyService {
+
+    // 權限判定時機點
+    // PreAuthorize 在執行前就確認是否有權限
+    // PostAuthorize 在執行後才確認是否有權限，沒權限就不回傳值，但是還是會執行到中間的步驟
 
     @PreAuthorize("hasRole('ADMIN')")
     String getMovie();
