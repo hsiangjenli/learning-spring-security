18929a179c24f2a93b054598c34afb4d7bdd21ba
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-23 17:40:52 +0800

    feat: 單元 4 - CSRF 的測試 - with(csrf())

--- lesson-6-2/src/test/java/com/kucw/security/MyTests.java
+++ lesson-6-2/src/test/java/com/kucw/security/MyTests.java
@@ -1,10 +1,15 @@
 package com.kucw.security;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -14,12 +19,44 @@ public class MyTests {
   private MockMvc mockMvc;
 
   @Test
-  public void testWelcome_noCsrfToken() throws Exception {}
+  public void testWelcome_noCsrfToken() throws Exception {
+
+    RequestBuilder requestBuilder = MockMvcRequestBuilders
+
+        .post("/welcome")
+
+        .with(httpBasic("test1", "111"));
+
+    mockMvc.perform(requestBuilder).andExpect(status().is(403));
+
+  }
 
   @Test
-  public void testWelcome_withCsrfToken() throws Exception {}
+  public void testWelcome_withCsrfToken() throws Exception {
+
+    RequestBuilder requestBuilder = MockMvcRequestBuilders
+
+        .post("/welcome")
+
+        .with(httpBasic("test1", "111"))
+
+        .with(csrf());
+
+    mockMvc.perform(requestBuilder).andExpect(status().is(200));
+
+  }
 
   @Test
-  public void testRegister_noCsrfToken() throws Exception {}
+  public void testRegister_noCsrfToken() throws Exception {
+
+    RequestBuilder requestBuilder = MockMvcRequestBuilders
+
+        .post("/register")
+
+        .with(httpBasic("test1", "111"));
+
+    mockMvc.perform(requestBuilder).andExpect(status().is(200));
+
+  }
 
 }
