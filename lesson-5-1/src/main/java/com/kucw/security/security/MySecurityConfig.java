2a309f70a59054edfad4e6d18b1d2c4a1806b1f7
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-23 09:39:59 +0800

    feat: 單元 2 - 解決 CORS 的方法（一）- Spring Security 的 cors 設定

--- lesson-5-1/src/main/java/com/kucw/security/security/MySecurityConfig.java
+++ lesson-5-1/src/main/java/com/kucw/security/security/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.kucw.security.security;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,40 +10,48 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails userTest1 = User
-                .withUsername("test1")
-                .password("{noop}111")
-                .roles("ADMIN", "USER")
-                .build();
+  @Bean
+  public InMemoryUserDetailsManager userDetailsManager() {
+    UserDetails userTest1 =
+        User.withUsername("test1").password("{noop}111").roles("ADMIN", "USER").build();
 
-        UserDetails userTest2 = User
-                .withUsername("test2")
-                .password("{noop}222")
-                .roles("USER")
-                .build();
+    UserDetails userTest2 = User.withUsername("test2").password("{noop}222").roles("USER").build();
 
-        return new InMemoryUserDetailsManager(userTest1, userTest2);
-    }
+    return new InMemoryUserDetailsManager(userTest1, userTest2);
+  }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        return http
-                .csrf(csrf -> csrf.disable())
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
+    return http.csrf(csrf -> csrf.disable()).httpBasic(Customizer.withDefaults())
+        .formLogin(Customizer.withDefaults())
 
-                .authorizeHttpRequests(request -> request
-                        .anyRequest().authenticated()
-                )
+        .authorizeHttpRequests(request -> request.anyRequest().authenticated())
 
-                .build();
-    }
+        .cors(cors -> cors.configurationSource(creatCorsConfigurationSource()))
+
+        .build();
+  }
+
+  private CorsConfigurationSource creatCorsConfigurationSource() {
+
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("*"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowedMethods(List.of("*"));
+    config.setMaxAge(3600L);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return source;
+  }
 }
