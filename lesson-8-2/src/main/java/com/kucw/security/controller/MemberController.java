595e495490c134cae67ae8faacb64f2c330c4e46
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-24 12:00:00 +0800

    feat: 單元 4 - 認證實作（三）- 密碼加密

--- lesson-8-2/src/main/java/com/kucw/security/controller/MemberController.java
+++ lesson-8-2/src/main/java/com/kucw/security/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.kucw.security.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
 import com.kucw.security.dao.MemberDao;
 import com.kucw.security.model.Member;
@@ -14,12 +15,20 @@ public class MemberController {
   @Autowired
   private MemberDao memberDao;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   @PostMapping("/register")
   public String register(@RequestBody Member member) {
+
+    String password = member.getPassword();
+    String encodedPassword = passwordEncoder.encode(password);
+    member.setPassword(encodedPassword);
 
     Integer memberId = memberDao.createMember(member);
 
     return "註冊成功";
+
   }
 
 
