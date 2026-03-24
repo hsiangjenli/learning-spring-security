0d15b83593c188c6ab74de4a3bc3101ad4e98a89
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-03-24 17:41:02 +0800

    feat: 單元 6 - 認證實作（五）- 單元測試

--- lesson-8-2/src/test/java/com/kucw/security/controller/MemberControllerTest.java
+++ lesson-8-2/src/test/java/com/kucw/security/controller/MemberControllerTest.java
@@ -1,11 +1,16 @@
 package com.kucw.security.controller;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kucw.security.model.Member;
 
 
 @AutoConfigureMockMvc
@@ -21,10 +26,33 @@ public class MemberControllerTest {
   @Test
   void testRegister() throws Exception {
 
+    Member member = new Member();
+    member.setAge(50);
+    member.setEmail("test1@gmail.com");
+    member.setMemberId(55555);
+    member.setName("test1");
+    member.setPassword("test1");
+
+    String json = objectMapper.writeValueAsString(member);
+    RequestBuilder requestBuilder = MockMvcRequestBuilders
+
+        .post("/register")
+
+        .header("Content-Type", "application/json")
+
+        .content(json);
+
+    mockMvc.perform(requestBuilder).andExpect(status().is(200));
   }
 
   @Test
   void testUserLogin() throws Exception {
+    RequestBuilder requestBuilder = MockMvcRequestBuilders
 
+        .post("/userLogin")
+
+        .with(httpBasic("normal@gmail.com", "normal"));
+
+    mockMvc.perform(requestBuilder).andExpect(status().is(200));
   }
 }
